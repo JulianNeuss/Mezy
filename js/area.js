438d912c5982a8699f40e9cf3b3e881bcbe37b7e
nicodelatorre7 <nicodelatorre7@gmail.com>
2019-10-06 11:15:07 -0300

    agrego pantalla para seleccionar pais y las banderas correspondientes, modifico href de area para que el next referencie a country

--- js/area.js
+++ js/area.js
@@ -31,10 +31,10 @@ Vue.component('areazone', {
                 <p class="display-2 font-weight-medium" align="center">CHOOSE AREA</p>
                 <br/>
                 <v-row>
-                    <v-col v-for="career in careers" :key="career"  cols="12" md="3" >
-                        <v-card :elevation="21" height="200" type="button" >  <!-- hay que sacar height y ponerlo en un css-->
+                    <v-col v-for="career in careers" :key="career.name"  cols="12" md="3" >
+                        <v-card flat height="200" type="button" >  <!-- hay que sacar height y ponerlo en un css-->
                             
-                            <v-img :src="career.src" height="170" class="orange--text"> </v-img>
+                            <v-img :src="career.src" height="170"> </v-img>
                             <h4  align="center"> {{career.name}}</h4>
                         </v-card>
                     </v-col>
@@ -44,14 +44,14 @@ Vue.component('areazone', {
     data() {
         return {
             careers: [
-                { name: 'Software Engineer', src:'src/software.jpg'},
-                { name: 'Mechanical Engineer', src:'src/mechanical.jpeg'},
-                { name: 'Industrial Engineer', src:'src/cds-industrial-engineering.jpg'},
-                { name: 'Navy Engineer', src:'src/navy.jpg'},
-                { name: 'Chemical Engineer', src:'src/chemical.jpg'},
-                { name: 'Civil Engineer', src:'src/civil.jpg'},
-                { name: 'Bio Engineer', src:'src/bio.png'},
-                { name: 'Oil Engineer', src:'src/petroleo.jpg'}
+                { name: 'Software Engineer', src:'../src/software.jpg'},
+                { name: 'Mechanical Engineer', src:'../src/mechanical.jpeg'},
+                { name: 'Industrial Engineer', src:'../src/cds-industrial-engineering.jpg'},
+                { name: 'Navy Engineer', src:'../src/navy.jpg'},
+                { name: 'Chemical Engineer', src:'../src/chemical.jpg'},
+                { name: 'Civil Engineer', src:'../src/civil.jpg'},
+                { name: 'Bio Engineer', src:'../src/bio.png'},
+                { name: 'Oil Engineer', src:'../src/petroleo.jpg'}
             ]
         }
     }
---
+++ js/country.js
@@ -0,0 +1,109 @@
+Vue.component('toolbar', {
+    template:
+        `<v-toolbar color="white" dark  flat >
+                      <!--  <v-btn color="white" depressed  href="index.html" >
+
+                        </v-btn> -->
+                        <v-spacer></v-spacer> <!-- deja espacio entre el search y boton de  notificaciones y settings -->
+
+
+                        <v-btn text color="black">
+                            HOW IT WORKS
+                        </v-btn>
+                        <v-btn text color="black">
+                            ABOUT US
+                        </v-btn>
+                        <v-btn text color="black">
+                            CONTACT US
+                        </v-btn>
+                        
+            </v-toolbar>`,
+
+})
+
+
+Vue.component('countryzone', {
+    template: `<div>
+                <v-btn icon href="area.html" >
+                    <v-icon large> arrow_back_ios </v-icon>
+                </v-btn>
+
+                <p class="display-2 font-weight-medium" align="center">SELECT COUNTRY</p>
+                <br/>
+                <v-row>
+                    <v-col v-for="flag in flags" :key="flag.country"  cols="12" md="3" >
+                        <v-card flat height="200" type="button" >  <!-- hay que sacar height y ponerlo en un css-->
+                            
+                            <v-img :src="flag.src" height="170"> </v-img>
+                            <h4  align="center"> {{flag.country}}</h4>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                
+                <br/>
+
+               
+                <v-row>
+                        <v-col v-show="show == false">
+                            <div class="text-center">
+                                <v-btn x-large color="primary" @click="show= !show">MORE COUNTRY'S</v-btn>
+                            </div>
+                        </v-col>
+                        
+                       
+                </v-row>
+                <v-row>
+                    <v-col v-show="show" v-for="more in moreflags" :key="more.country"  cols="12" md="3" >
+                        <v-card flat height="200" type="button" >  <!-- hay que sacar height y ponerlo en un css-->
+                            
+                            <v-img :src="more.src" height="170"> </v-img>
+                            <h4  align="center"> {{more.country}}</h4>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                        <v-col v-show="show">
+                            <div class="text-center">
+                                <v-btn x-large color="primary" @click="show= !show">LESS COUNTRY'S</v-btn>
+                            </div>
+                        </v-col>
+                </v-row>
+                
+                
+             
+            </div>`,
+    data() {
+        return {
+            show: false,
+            flags: [
+                { country: 'France', src:'../src/france.png'},
+                { country: 'Italy', src:'../src/italy.svg'},
+                { country: 'USA', src:'../src/USA.png'},
+                { country: 'China', src:'../src/china.png'},
+                { country: 'Australia', src:'../src/australia.svg'},
+                { country: 'Japan', src:'../src/japan.svg'},
+                { country: 'Germany', src:'../src/germany.svg'},
+                { country: 'United Kingdom', src:'../src/UK.png'}
+            ],
+            moreflags: [
+                { country: 'Netherland', src:'../src/netherland.png'},
+                { country: 'South Korea', src:'../src/korea.png'},
+                { country: 'Denmark', src:'../src/denmark.png'},
+                { country: 'Chile', src:'../src/chile.png'},
+                { country: 'Brazil', src:'../src/brazil.png'},
+                { country: 'Belgium', src:'../src/belgium.png'},
+            ]
+        }
+    }
+})
+
+
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data () {
+        return {
+            title: 'Your Logo'
+        }
+    }
+})
