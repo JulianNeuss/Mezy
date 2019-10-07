a41ceba840a58e444d1b117ce32b27f35069cf08
juan <olivajuan12@gmailc>
2019-10-07 20:54:14 -0300

    node server structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://localhost/mezy-database')
+    .then(db => console.log('DB is connected'))
+    .catch(err =>console.error(err));
+
+// Settings
+app.set('port', 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+// app.use('/tasks', require('./routes/tasks'));
+
+// Static files
+app.use(express.static(__dirname + '/public'));
+
+// Server is listening
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
