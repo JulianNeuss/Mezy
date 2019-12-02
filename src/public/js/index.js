8f593bc987c7e6ae31d23cff236c8e7da5578181
Julian Neuss <julineuss@gmail.com>
2019-12-02 19:09:53 -0300

    News

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -95,6 +95,78 @@ Vue.component('howitworks', {
         </section>`,
 
 }),
+Vue.component('news', {
+  template:
+      `<section>
+                    <v-layout
+                        column
+                        wrap
+                        class="my-12"
+                        align-center
+                    >
+                    <v-flex xs12 sm4 class="my-4">
+                        <div class="text-center">
+                            <h2 class="headline">Top Tier Universities</h2>
+                        </div>
+                    </v-flex>
+                    
+                    <v-flex xs12>
+                    <v-container grid-list-xl>
+                    <v-layout row wrap align-center>
+                    <v-flex xs12 md4>
+                    <v-card flat class="transparent">
+                       
+                       <template>
+                        <v-row align="center" justify="center">
+                                <v-img
+                                  :src='../assets/Wuhan.png'
+                               class='imgRedonda' type="button"
+                                    ></v-img>
+                         </v-row>
+                      </template>
+                  
+                    <v-card-text>
+                    Why students go more to USA?
+                    </v-card-text>
+                    </v-card>
+                    </v-flex>
+                    <v-flex xs12 md4>
+                    <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                        <v-icon x-large class="blue--text text--lighten-2">mdi-flash</v-icon>
+                        </v-card-text>
+                        <v-card-title primary-title class="layout justify-center">
+                        <div class="headline">Build your schedule</div>
+                    </v-card-title>
+                    <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                    </v-card-text>
+                    </v-card>
+                    </v-flex>
+                    <v-flex xs12 md4>
+                    <v-card flat class="transparent">
+                        <v-card-text class="text-center">
+                        <v-icon x-large class="blue--text text--lighten-2">mdi-wrench</v-icon>
+                        </v-card-text>
+                        <v-card-title primary-title class="layout justify-center">
+                        <div class="headline text-center">Travel & Learn</div>
+                        </v-card-title>
+                        <v-card-text>
+                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                        Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                    </v-card-text>
+                    </v-card>
+                    </v-flex>
+                    </v-layout>
+                    </v-container>
+                    </v-flex>
+                    </v-layout>
+        </section>`,
+
+}),
 Vue.component('parallax2', {
     template:
         `<section>
