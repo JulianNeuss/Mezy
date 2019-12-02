a0f8dbe85ccb8d978914df3db4b25c56d1fa5fb3
juan <olivajuan12@gmailc>
2019-12-01 22:34:31 -0300

    index.html

--- src/models/course.js
+++ src/models/course.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema;
 
 var course = new Schema({
     _id: mongoose.Types.ObjectId,
-    uni: String,
-    carrera: String,
+    uni_id: Number,
+    carrera_id: Number,
     materia_externa: String,
     materia_itba: String,
     creditos_electiva: Number,
