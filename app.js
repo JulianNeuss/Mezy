b90d8dd006d21d29536d75c1a0881332570a2b06
juan <olivajuan12@gmailc>
2019-12-03 10:17:23 -0300

    server listen

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ app.get('/', (req,res) => {
 });
 
 // Server is listening
-// app.listen(process.env.port || app.get('port'), () => {
-//     console.log('Server on port', app.get('port'));
-// })
+ app.listen(process.env.port || app.get('port'), () => {
+     console.log('Server on port', app.get('port'));
+ })
+/* 
 
-
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+         console.log('Server on port', app.get('port'));
+     }) */
